2e4ca66756930604b57a2cdf248b3c85b9903987
jobbol <lobonthejob@gmail.com>
2023-11-07 17:33:29 -0600

    Appearance improvements.

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,7 +1,23 @@
-export default function Menu ({alterPage}) {
+import './Menu.css';
+export default function Menu ({alterPage, alterSettings}: any) {
     return <main>
-        <h1>Blackjack</h1>
-        <button>Single Player</button>
-        <button>Multiplayer</button>
+        <h1>
+            <small>♦ ♠ ♥ ♣</small>
+            <span>Blackjack</span>
+            <small>♣ ♥ ♠ ♦</small>
+        </h1>
+        {/* <div id="suits">♦ ♥ ♠ ♣</div> */}
+        <button
+            className="btn-secondary"
+            onClick={() => {
+                alterSettings({hasDealer: false});
+                alterPage('game');
+        }}>Multiplayer</button>
+        <button 
+            className="btn-primary btn-game"
+            onClick={() => {
+                alterSettings({hasDealer: true});
+                alterPage('game');
+        }}>Single Player</button>
     </main>;
 }
