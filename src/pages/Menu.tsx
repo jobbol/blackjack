d6acc1266b7d9a529a70721331c148cabd411ea8
lobcog <lobonthejob@gmail.com>
2023-11-10 01:18:55 -0600

    Fixed background changer

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,32 +1,59 @@
 import './Menu.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
+
+/**
+ * Main menu page.  Displays the title and lets user choose between multiplayer or single player.
+ */
 export default function Menu ({alterPage, alterSettings}: any) {
-    const suitsLeft = ['♦','♠','♥','♣'].map((char: string, i: number) => {
-        return <span className={i % 2? 'black' : 'red'} key={char}>{char}</span>
-    });
-    const suitsRight = ['♣','♥','♠','♦'].map((char: string, i: number) => {
-        return <span className={i % 2? 'red' : 'black'} key={char}>{char}</span>
+    return <>
+        <div className="vertical-center">
+            <h1>
+                <Suits direction="left"/>
+                <span>Blackjack</span>
+                <Suits direction="right"/>
+            </h1>
+            <div id="button-container">
+                <button
+                    className="btn btn-secondary"
+                    onClick={() => {
+                        alterSettings({hasDealer: false});
+                        alterPage('game');
+                }}>Multiplayer</button>
+                <button 
+                    id="btn-game"
+                    className="btn btn-primary"
+                    onClick={() => {
+                        alterSettings({hasDealer: true});
+                        alterPage('game');
+                }}>Single Player</button>
+            </div>
+        </div>
+    </>;
+}
+
+
+
+/**
+ * Creates a decorative element of suit icons to be placed on the sides of the game name.
+ * @param {'left' | 'right'} direction
+ */
+function Suits ({direction}: {direction: 'left' | 'right'}) {
+    let suits;
+    if (direction === 'left') {
+        suits = ['♦','♠','♥','♣'];
+    } else {
+        suits = ['♣','♥','♠','♦'];
+    }
+
+    let suitsJSX = suits.map((char: string, i: number) => {
+        let className;
+        if (direction === 'left') {
+            className = i % 2? 'black' : 'red';
+        } else {
+            className = i % 2? 'red': 'black';
+        }
+        return <span className={className} key={char}>{char}</span>
     });
 
-    return <main>
-        <h1>
-            <small>{suitsLeft}</small>
-            <span>Blackjack</span>
-            <small>{suitsRight}</small>
-        </h1>
-        <div className="jumbotron">asdfjasdfj</div>
-        <button
-            className="button btn-secondary"
-            onClick={() => {
-                alterSettings({hasDealer: false});
-                alterPage('game');
-        }}>Multiplayer</button>
-        <button 
-            className="button btn-primary btn-game"
-            onClick={() => {
-                alterSettings({hasDealer: true});
-                alterPage('game');
-        }}>Single Player</button>
-    </main>;
+    return <small>{suitsJSX}</small>
 }
